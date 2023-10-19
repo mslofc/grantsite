ded301fd86db7a6891c4e8deb2b6d6cbf8c0150c
msl <paulorngu@gmail.com>
2023-10-19 01:27:44 -0700

    updated

--- register.php
+++ register.php
@@ -3,36 +3,51 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-session_start(); // Start the session at the beginning of the script
+session_start();
 
-include('db_connect.php'); // Include your database connection script
+include('db_connect.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve user input from the form
     $username = mysqli_real_escape_string($conn, $_POST["username"]);
+    $email = mysqli_real_escape_string($conn, $_POST["email"]); // Add email handling
     $password = $_POST["password"];
-    
-    // Validate input (you can add more validation as needed)
-    if (empty($username) || empty($password)) {
-        echo "Username and password are required.";
-    } else {
-        // Hash the password securely
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-        // Insert user data into the database
-        $sql = "INSERT INTO users (username, password) VALUES ('$username', '$hashed_password')";
-        
-        if ($conn->query($sql) === TRUE) {
-            // Registration successful
-            $_SESSION["username"] = $username;
-            echo "Registration successful! Welcome, $username!";
+    if (empty($username) || empty($password) || empty($email)) {
+        echo "Username, email, and password are required.";
+    } else {
+        // Check for duplicate email
+        $checkQuery = "SELECT COUNT(*) FROM users WHERE email = ?";
+        $stmt = $conn->prepare($checkQuery);
+        $stmt->bind_param('s', $email);
+        $stmt->execute();
+        $stmt->bind_result($count);
+        $stmt->fetch();
+        $stmt->close();
+
+        if ($count > 0) {
+            // Email is already in use, provide an error message to the user
+            echo "Email address is already in use.";
         } else {
-            // Registration failed
-            echo "Error: " . $sql . "<br>" . $conn->error;
+            // Hash the password securely
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+            // Insert user data into the database
+            $sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param('sss', $username, $email, $hashed_password);
+
+            if ($stmt->execute()) {
+                // Registration successful
+                $_SESSION["username"] = $username;
+                echo "Registration successful! Welcome, $username!";
+            } else {
+                // Registration failed
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+            $stmt->close();
         }
     }
 }
 
-// Close the database connection
 $conn->close();
 ?>
